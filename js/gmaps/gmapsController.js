7fe77ae3b5022f62ed5a008da51d5a19bab9a341
Pedro Silva <pedrro2@hotmail.com>
2016-04-17 16:50:21 -0300

    Adding queries to googlemap api

--- js/gmaps/gmapsController.js
+++ js/gmaps/gmapsController.js
@@ -2,20 +2,37 @@ angular.module('gmapsPOC').controller('gmapsController', gmapsController);
 
 function gmapsController($scope, gmapsRepository) {
   $scope.places = [];
+  var placeCoord = {};
+  var place_id;
 
-
-  $scope.searchPlaces = function() {
-    if($scope.places.length > 0){
+  var searchNearbyPlacesByCoord = function(lat, long) {
+    if ($scope.places.length > 0) {
       $scope.places = [];
     }
-    gmapsRepository.getNearbyPlaces($scope.latitude,$scope.longitude).then(function (response) {
+    gmapsRepository.getNearbyPlaces(lat, long).then(function(response) {
       $scope.places.push(response.data.results);
     });
   };
 
-  // $scope.picturesFromPlaces = function(reference){
-  //   gmapsRepository.getPictureFromPlace(reference).then(function (response) {
-  //     return response;
-  //   });
-  //};
+  var getDetailsFromPlace = function(place_id) {
+    gmapsRepository.getCoordFromPlace(place_id).then(function(response) {
+      placeCoord = {
+        latitude: response.data.result.geometry.location.lat,
+        longitude: response.data.result.geometry.location.lng
+      };
+      searchNearbyPlacesByCoord(placeCoord.latitude, placeCoord.longitude);
+    });
+  };
+
+  $scope.searchPlacesByName = function() {
+    gmapsRepository.getIdFromPlace($scope.name).then(function(response) {
+      getDetailsFromPlace(response.data.predictions[0].place_id);
+    });
+  };
+
+  $scope.picturesFromPlaces = function(reference) {
+    gmapsRepository.getPictureFromPlace(reference).then(function(response) {
+      return response;
+    });
+  };
 }
--- js/gmaps/gmapsFactory.js
+++ js/gmaps/gmapsFactory.js
@@ -1,11 +1,25 @@
 angular.module('gmapsPOC').factory('gmapsRepository', function($http) {
+  var api_key = "AIzaSyDNGPh2ERYJq9Ei1tzDSNG-nOyYAJVhpY4";
+  var url = "";
   return {
-    getNearbyPlaces: function(lat, long) {
-        var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat+","+long+"&radius=300&language=pt-PT&key=AIzaSyDNGPh2ERYJq9Ei1tzDSNG-nOyYAJVhpY4";
+    //https://developers.google.com/places/web-service/autocomplete#place_autocomplete_responses
+    getIdFromPlace: function(name) {
+      url = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=" + name + "&key=" + api_key;
       return $http.get(url);
     },
+    //https://developers.google.com/places/web-service/details#PlaceDetailsRequests
+    getCoordFromPlace: function(placeid) {
+      url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + placeid + "&key=" + api_key;
+      return $http.get(url);
+    },
+    //https://developers.google.com/places/web-service/search#PlaceSearchRequests
+    getNearbyPlaces: function(lat, long) {
+      url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=300&language=pt-PT&key=" + api_key;
+      return $http.get(url);
+    },
+    //https://developers.google.com/places/web-service/photos#photo_references
     getPictureFromPlace: function(photoReference) {
-      var url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference="+photoReference+"&key=AIzaSyDNGPh2ERYJq9Ei1tzDSNG-nOyYAJVhpY4";
+      url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + photoReference + "&key=" + api_key;
       return $http.get(url);
     }
   };
